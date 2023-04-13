3f34e13348d27a0030adc224fd323b75d9ab9788
arunramalingam1 <arunramalingam@1992@gmail.com>
2023-04-14 00:03:05 +0530

    included

--- src/test/java/com/runner/Testrunnerclass.java
+++ src/test/java/com/runner/Testrunnerclass.java
@@ -17,13 +17,19 @@ import io.cucumber.junit.CucumberOptions.SnippetType;
 @RunWith(Cucumber.class)
 @CucumberOptions(tags = "", dryRun = false, features = "src/test/resources/Features", plugin = { "pretty",
 		"json:target/index.json" }, monochrome = true, stepNotifications = true, snippets = SnippetType.CAMELCASE, glue = "com.stepdefinition")
+//This is test runner class
+/**
+ * 
+ * @author arun
+ *
+ */
 public class Testrunnerclass extends BaseClass {
 	/**
 	 * @see used to Generate Snippets and excecute step definition
 	 * @throws FileNotFoundException
 	 * @throws IOException
 	 */
-// This is test runner class
+
 	@AfterClass
 	public static void afterClass() throws FileNotFoundException, IOException {
 		Reporting.generateJvmreport(getProjectPath() + getPropertyfilevalue("jsonpath"));
